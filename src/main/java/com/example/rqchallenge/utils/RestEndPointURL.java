eb217a2a033fa191db37b4e1e8e8912df541acf7
Bhavesh <bhaveshpandya52@gmail.com>
2024-04-05 16:12:00 +0530

    commit

---
+++ src/main/java/com/example/rqchallenge/utils/RestEndPointURL.java
@@ -0,0 +1,8 @@
+package com.example.rqchallenge.utils;
+
+public class RestEndPointURL {
+    public static final String GET_ALL_EMPLOYEE = "http://dummy.restapiexample.com/api/v1/employees";
+    public static final String GET_EMPLOYEE_BY_ID = "http://dummy.restapiexample.com/api/v1/employee/{id}";
+    public static final String CREATE_NEW_EMPLOYEE = "http://dummy.restapiexample.com/api/v1/create";
+    public static final String DELETE_EMPLOYEE_BY_ID = "http://dummy.restapiexample.com/api/v1/delete/{id}";
+}
